2b92cd4c7c83b5f8a44de6f5334f30670d92d9b5
motatan79 <m.pirela@gmail.com>
2023-12-11 22:37:22 -0300

    Clase Finalizada

---
+++ project/config/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+
+def saludo(request) -> HttpResponse:
+    return HttpResponse("¡Hola!")
+
+def ingresar_nombre(request) -> HttpResponse:
+    nombre = input('Ingresa tu nombre: ')
+    return HttpResponse(f'Tu nombre es {nombre}')
+
+def nombre(request, nombre):
+    return HttpResponse(nombre)
+
+def fecha_hora(request):
+    from datetime import datetime
+    ahora = datetime.now().strftime(r'"%d/%m/%Y %H:%M:%S.%f')
+    return HttpResponse(ahora)
+
+def tirar_dados(request):
+    import random
+    numero = random.randint(1,6)
+    if numero == 6:
+        return HttpResponse('Felicitaciones obtuviste el numero 6')
+    else: 
+        return HttpResponse('Vuelve a intentar')
